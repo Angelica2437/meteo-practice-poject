907b010b92d881bde566933bbac91f2656171ca9
Angelica2437 <m.moralesangelica@gmail.com>
2024-07-08 15:35:00 +0200

    added search API

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#weather-app-temperature");
+  let temperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(temperature);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+}
+function searchCity(city) {
+  let apiKey = "638of0ec2431000tb70ab50b836c7a7f";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
 function handleSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearch);
